68e7e82c0aab9850b4ea00761df81f0fb14c6330
Unknown <eliocampitelli@gmail.com>
2019-05-23 17:01:03 -0300

    First commit with something

---
+++ R/hello.R
@@ -0,0 +1,24 @@
+pkg <- "ggplot2"
+pkg_author <- packageDescription(pkg, field = "Maintainer")
+
+
+pkg_mail <- stringi::stri_match_all(pkg_author, regex = "<(.*?)>")[[1]][2]
+pkg_author <- stringi::stri_replace_all(pkg_author, replacement = "", regex = "<(.*?)>")
+pkg_author <- stringi::stri_trim(pkg_author)
+
+subject <- paste0("Thanks for the ", pkg, " package!")
+
+body <- paste("Hi, ", pkg_author, ",\n",
+              "I'm an R user and just wanted to tell you that I love your ", pkg, " R package.\n",
+              "That's all. Have a nice day!")
+
+mail <- callr::r_vanilla(function(pkg_mail, subject, body) {
+
+  mailto <- paste0("mailto:", pkg_mail,
+                   "?subject=", subject,
+                   "?body=", body)
+  browseURL(mailto)
+}, args = list(pkg_mail = pkg_mail,
+               subject = subject,
+               body = body))
+
